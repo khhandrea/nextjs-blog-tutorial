bb7da55cda2143c141a01566650f047b1572f57d
khhandrea <khhandrea@naver.com>
2021-04-29 17:47:09 +0900

    Add blog and blog  url to index

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -33,12 +34,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              Title:{' '}{title}
-              <br />
-              Id:{' '}{id}
-              <br />
-              Date:{' '}{date}
-            </li>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
